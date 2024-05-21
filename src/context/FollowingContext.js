45ef537750928550fa3f26046bc812dc63b1b910
unknown <ejbiasi@hotmail.com>
2024-05-21 03:31:21 -0300

    matutando os follows

--- src/context/FollowingContext.js
+++ src/context/FollowingContext.js
@@ -3,15 +3,22 @@ import React, { createContext, useState, useContext } from "react";
 const FollowingContext = createContext();
 
 export const FollowingProvider = ({ children }) => {
-  const [followingInfo, setFollowingInfo] = useState(false);
+  const [followingInfo, setFollowingInfo] = useState([]);
+  const [following, setFollowing] = useState(false);
 
   const updateFollowingInfo = (newFollowingInfo) => {
     console.log("newFollowingInfo no FollowingCONTEXT: ", newFollowingInfo);
     setFollowingInfo(newFollowingInfo);
   };
 
+  const updateFollowing = (bool) => {
+    setFollowing(bool);
+  };
+
   return (
-    <FollowingContext.Provider value={{ followingInfo, updateFollowingInfo }}>
+    <FollowingContext.Provider
+      value={{ followingInfo, updateFollowingInfo, following, updateFollowing }}
+    >
       {children}
     </FollowingContext.Provider>
   );
